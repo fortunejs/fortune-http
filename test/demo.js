86d780994bc840c178c99007ca6c8376ddd6b3d2
0x8890 <0x8890@airmail.cc>
2016-11-14 13:30:52 +0100

    add script to run demo

--- test/demo.js
+++ test/demo.js
@@ -3,25 +3,34 @@
 const http = require('http')
 const util = require('util')
 const chalk = require('chalk')
+const loremIpsum = require('lorem-ipsum')
 const instance = require('fortune/test/integration/test_instance')
 const createListener = require('../lib')
 
 const port = 1337
 
 instance().then(store => {
+  store.options.settings.name = 'Fortune Test Web Service'
+
   /* eslint-disable max-len */
-  store.options.settings.name = 'Foobar'
-  store.options.settings.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eleifend tempor vulputate. Etiam rhoncus vulputate leo nec accumsan. Etiam eleifend ultrices faucibus. Vestibulum quis sem neque.'
-  store.options.documentation = {
-    user: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    name: {
-      en: 'Vestibulum vitae velit id tellus vehicula dictum nec at ante. Sed pulvinar eros quis volutpat volutpat. Integer a pulvinar tellus, ut vehicula diam.'
-    }
-  }
+  store.options.settings.description = '<p>This is a demo of the default HTML interface of <code>fortune-http</code>. This interface exposes most of the features of Fortune.js. Notably, it does not require JavaScript to use.</p>'
   /* eslint-enable max-len */
 
-  store.on(store.common.events.change, data => console.log(chalk.cyan(
-    `${chalk.bold('Change')}: ${util.inspect(data, { depth: null })}`)))
+  store.options.documentation = {}
+
+  const opts = {
+    count: 3,
+    units: 'sentences'
+  }
+
+  for (const type in store.recordTypes) {
+    store.options.documentation[type] = loremIpsum(opts)
+    for (const field in store.recordTypes[type])
+      store.options.documentation[field] = loremIpsum(opts)
+  }
+
+  /*store.on(store.common.events.change, data => console.log(chalk.cyan(
+    `${chalk.bold('Change')}: ${util.inspect(data, { depth: null })}`)))*/
 
   const listener = createListener(store)
   const server = http.createServer((request, response) =>
@@ -31,5 +40,5 @@ instance().then(store => {
     }))
 
   server.listen(port, () =>
-    console.log(chalk.blue(`Listening on port ${port}...`)))
+    console.log(`# ${chalk.green(`Listening on port ${port}...`)}`))
 })
