ee94c4467194d2b182339e1fd0c34cab38f6916d
Dali Zheng <d@liwa.li>
2017-07-22 15:45:22 -0700

    add checks for abusive requests

--- test/http.js
+++ test/http.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const zlib = require('zlib')
+const http = require('http')
 const run = require('tapdance')
+const testInstance = require('fortune/test/integration/test_instance')
+const createListener = require('../lib')
 const httpTest = require('./http_test')
 const test = httpTest.bind(null, void 0)
 
@@ -24,7 +27,60 @@ run((assert, comment) => {
     assert(response.headers['content-encoding'] === 'gzip', 'encoding is correct')
 
     // We're expecting JSON as the default content type.
-    JSON.parse(String(zlib.gunzipSync(Buffer.from(response.body))))
-    assert(true, 'response compressed correctly')
+    const body = JSON.parse(String(zlib.gunzipSync(Buffer.from(response.body))))
+    assert(Object.keys(body).length, 'response compressed correctly')
   })
 })
+
+run((assert, comment) => {
+  comment('request rate check')
+
+  return failRequest({ 'Content-Length': '1' })
+    .then(response => {
+      assert(response.statusCode === 408, 'request timed out')
+    })
+})
+
+run((assert, comment) => {
+  comment('request size check')
+
+  return failRequest({})
+    .then(response => {
+      assert(response.statusCode === 411, 'content-length required')
+      return failRequest({
+        'Content-Length': 2 * Math.pow(2, 20) + 1
+      })
+    })
+    .then(response => {
+      assert(response.statusCode === 413, 'request too large')
+    })
+})
+
+
+function failRequest (headers) {
+  let server
+
+  return testInstance()
+    .then(store => {
+      server = http.createServer((request, response) => {
+        createListener(store, {
+          settings: {
+            // For forcing the test to time out faster.
+            rateCheckMS: 100
+          }
+        })(request, response).catch(() => null)
+      })
+      return new Promise(resolve => server.listen(resolve))
+    })
+    .then(() => {
+      let port = server.address().port
+      let httpRequest = http.request({
+        port, path: '/', method: 'POST', headers
+      })
+
+      httpRequest.flushHeaders()
+
+      return new Promise(resolve =>
+        httpRequest.on('response', resolve))
+    })
+}
--- test/http_test.js
+++ test/http_test.js
@@ -17,87 +17,80 @@ const fortune = require('fortune')
 const testInstance = require('fortune/test/integration/test_instance')
 const stderr = require('fortune/test/stderr')
 
-let i = 0
-
 
 module.exports = function httpTest (options, path, request, fn, change) {
   let store
   let server
-  let port = 1024 + i
-
-  i++
-  if (port >= 65535) i = 0
 
   return testInstance()
+    .then(instance => {
+      store = instance
 
-  .then(instance => {
-    store = instance
+      if (typeof change === 'function')
+        store.on(fortune.change, data => change(data, fortune.methods))
 
-    if (typeof change === 'function')
-      store.on(fortune.change, data => change(data, fortune.methods))
+      server = (options && options.uws ? uws.http : http)
+        .createServer((request, response) =>
+          createListener(store, options)(request, response)
+            .catch(error => stderr.error(error)))
 
-    const listener = createListener(store, options)
-
-    server = (options && options.uws ? uws.http : http)
-    .createServer((request, response) => {
-      listener(request, response)
-      .catch(error => stderr.error(error))
+      return new Promise(resolve => server.listen(resolve))
     })
-    .listen(port)
+    .then(() => {
+      let port = server.address().port
+      let headers
+      let status
 
-    let headers
-    let status
-
-    if (request) {
-      if ('method' in request) request.method = request.method.toUpperCase()
-      if (!request.headers) request.headers = {}
-      if (typeof request.body === 'object') {
-        request.body = JSON.stringify(request.body)
-        request.headers['content-length'] = Buffer.byteLength(request.body)
+      if (request) {
+        if ('method' in request) request.method = request.method.toUpperCase()
+        if (!request.headers) request.headers = {}
+        if (typeof request.body === 'object') {
+          request.body = JSON.stringify(request.body)
+          request.headers['content-length'] = Buffer.byteLength(request.body)
+        }
       }
-    }
-    else request = { headers: {} }
-    request.headers['connection'] = 'keep-alive'
+      else request = { headers: {} }
+      request.headers['connection'] = 'keep-alive'
 
-    return new Promise((resolve, reject) =>
-      http.request(Object.assign({ port, path }, request), response => {
-        headers = response.headers
-        status = response.statusCode
+      return new Promise((resolve, reject) =>
+        http.request(Object.assign({ port, path }, request), response => {
+          headers = response.headers
+          status = response.statusCode
 
-        const chunks = []
+          const chunks = []
 
-        response.on('error', reject)
-        response.on('data', chunk => chunks.push(chunk))
-        response.on('end', () => resolve(Buffer.concat(chunks)))
-      }).end(request ? request.body : null))
+          response.on('error', reject)
+          response.on('data', chunk => chunks.push(chunk))
+          response.on('end', () => resolve(Buffer.concat(chunks)))
+        }).end(request ? request.body : null))
 
-    .then(response => {
-      // uws server seems to be temporal.
+      .then(response => {
+        // uws server seems to be temporal.
+        if (server) server.close()
+
+        stderr.debug(chalk.bold('Response status: ' + status))
+        stderr.debug(headers)
+        return store.disconnect().then(() => response)
+      })
+
+      .then(body => {
+        if (body.length)
+          try {
+            body = JSON.parse(body.toString())
+            stderr.log(body)
+          }
+          catch (error) {
+            // If it couldn't be parsed as JSON, not a problem.
+            stderr.warn(body)
+          }
+
+        return fn({ status, headers, body })
+      })
+    })
+
+    .catch(error => {
+      stderr.error(error)
       if (server) server.close()
-
-      stderr.debug(chalk.bold('Response status: ' + status))
-      stderr.debug(headers)
-      return store.disconnect().then(() => response)
+      throw error
     })
-
-    .then(body => {
-      if (body.length)
-        try {
-          body = JSON.parse(body.toString())
-          stderr.log(body)
-        }
-        catch (error) {
-          // If it couldn't be parsed as JSON, not a problem.
-          stderr.warn(body)
-        }
-
-      return fn({ status, headers, body })
-    })
-  })
-
-  .catch(error => {
-    stderr.error(error)
-    if (server) server.close()
-    throw error
-  })
 }
